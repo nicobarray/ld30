731532160f2dd2c5ca0e1b15689cc471075d84ac
Nicolas Barray <nbarray@gmail.com>
2014-08-23 15:33:00 +0200

    Main game loop written

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -10,7 +10,68 @@ GameEngine::~GameEngine(void)
 
 }
 
-int main(void)
+void GameEngine::update()
 {
-	return 0;
+
+}
+
+// Blit sprites and stuffs on the window's surface
+void GameEngine::draw(SDL_Surface* screen)
+{
+	
+}
+
+bool GameEngine::quit_get()
+{
+	return false;
+}
+
+int main(int argc, char** argv)
+{
+	bool success = true;
+	bool quit = false;
+	SDL_Window* window = nullptr;
+	SDL_Surface* screen = nullptr;
+	SDL_Event e;
+
+	// Our game engine
+	GameEngine engine = GameEngine();
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cerr << "SDL Init error: " << SDL_GetError() << std::endl;
+		success = false;
+	}
+	else
+	{
+		GameEngine engine = GameEngine();
+		window = SDL_CreateWindow("ld30", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_SHOWN);
+
+		if (!window)
+		{
+			std::cerr << "SDL Create Window error : " << SDL_GetError() << std::endl;
+			success = false;
+		}
+		else
+			screen = SDL_GetWindowSurface(window);
+	}
+
+	if (success)
+	{
+		while (!quit)
+		{
+			while (SDL_PollEvent(&e) != 0)
+			{
+				if (e.type == SDL_QUIT)
+					quit = true;
+				engine.update();
+			}
+
+			engine.draw(screen);
+			SDL_UpdateWindowSurface(window);
+		}
+	}
+
+	SDL_Quit();
+	return !success;
 }
--- GameEngine.h
+++ GameEngine.h
@@ -8,5 +8,12 @@ class GameEngine
 public:
 	GameEngine(void);
 	~GameEngine(void);
+
+	void update();
+	void draw(SDL_Surface* screen);
+
+	bool quit_get();
+
+private:
 };
 
