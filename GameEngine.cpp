95e1c1d1eb924e025c055e6f761aa88412b4803f
Nicolas Barray <nbarray@gmail.com>
2014-08-23 14:41:07 +0200

    GameEngine class added

---
+++ GameEngine.cpp
@@ -0,0 +1,16 @@
+#include "GameEngine.h"
+
+GameEngine::GameEngine(void)
+{
+
+}
+
+GameEngine::~GameEngine(void)
+{
+
+}
+
+int main(void)
+{
+	return 0;
+}
---
+++ GameEngine.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<SDL.h>
+#include<iostream>
+
+class GameEngine
+{
+public:
+	GameEngine(void);
+	~GameEngine(void);
+};
+
