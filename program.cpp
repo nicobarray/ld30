a16444ef4a307de685849694663d53e510c09c23
Nicolas Barray <nbarray@gmail.com>
2014-08-23 14:23:38 +0200

    Test de synchro

--- program.cpp
+++ program.cpp
@@ -8,15 +8,13 @@ program::~program(void)
 {
 }
 
+
+// toto
+
 int main(int argc, char **argv){
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
-	}
-
-	while (true)
-	{
-
 	}
 
 	SDL_Quit();
--- program.h
+++ program.h
@@ -7,4 +7,7 @@ class program
 public:
 	program(void);
 	~program(void);
+	void initialize();
+	void update();
+	void draw(SDL_Surface* screen);
 };
